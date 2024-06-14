3f9a3e40df2fc723bfcc4bf4fd27c8142348800c
NIKOOO2528 <149268280+NIKOOO2528@users.noreply.github.com>
2024-06-14 18:09:08 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendCharacter(character) {
+    document.getElementById('display').value += character;
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
